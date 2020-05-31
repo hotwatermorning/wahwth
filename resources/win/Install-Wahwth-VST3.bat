22971928c433c041b687f30a52be02e6529e49e0
hotwatermorning <hotwatermorning@gmail.com>
2020-05-31 22:07:11 +0900

    [resource] fix install script

--- resources/win/Install-Wahwth-VST3.bat
+++ resources/win/Install-Wahwth-VST3.bat
@@ -2,9 +2,12 @@
 setlocal
 
 pushd "%~dp0"
+echo prepare installation directory
 mkdir "C:\Program Files\Common Files\VST3"
+echo remove old file
 del /q "C:\Program Files\Common Files\VST3\Wahwth.vst3"
-copy .\Wahwth.vst3 "C:\Program Files\Common Files\VST3"
+echo copy plugin file
+xcopy "Wahwth.vst3" "C:\Program Files\Common Files\VST3" /Y /E /I
 popd
 
 echo "インストールが完了しました"
